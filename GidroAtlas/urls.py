c6c694ca87d950cc9811e6e30cf6774837d38847
Panda <blackneterro@gmail.com>
2025-12-07 12:50:52 +0500

    panda

--- GidroAtlas/urls.py
+++ GidroAtlas/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from GidroAtlas import views
 
@@ -28,3 +30,5 @@ urlpatterns = [
     path('user/', include('User.urls')),
     path('atla/', include('Atla.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
